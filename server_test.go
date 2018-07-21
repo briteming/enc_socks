efaa4ba1f67b0d155ce99335956e6a4b4e0cba94
senxxc <naivesen@gmail.com>
2018-07-21 19:54:53 +0800

    remove key config

--- server_test.go
+++ server_test.go
@@ -11,8 +11,7 @@ func TestServer(t *testing.T) {
     mp := relay.NewAuthMap()
     mp.Add(relay.NewAuthMsg("xxxsen", "hello test"))
     cfg := &ServerConfig{
-        LocalAddr:"127.0.0.1:8848", RemoteAddr:addr,
-        Key:"hello_world", Timeout:3 * time.Second, UserInfo:*mp, ServerType:SERVER_TYPE_REMOTE,
+        LocalAddr:"127.0.0.1:8848", RemoteAddr:addr, Timeout:3 * time.Second, UserInfo:*mp, ServerType:SERVER_TYPE_REMOTE,
         TlsServerPemAddr:"/home/sen/GoPath/src/enc_socks/cmd/server.pem", TlsServerKeyAddr:"/home/sen/GoPath/src/enc_socks/cmd/server.key"}
     svr := NewRelayServer(cfg)
     svr.Start()
