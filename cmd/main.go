1a5651fa10c62e30ee795b201701993eb6afe474
senxxc <naivesen@gmail.com>
2018-07-22 11:44:29 +0800

    日志输出格式

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,17 @@ import (
     "time"
     "enc_socks"
     "enc_socks/relay"
+    "os"
 )
 
-var flagLocal = flag.String("local", "127.0.0.1:8848", "local bind addr")
-var flagRemote = flag.String("remote", "127.0.0.1:8849", "remote server addr")
+var flagLocal = flag.String("local", "127.0.0.1:8847", "local bind addr")
+var flagRemote = flag.String("remote", "127.0.0.1:8848", "remote server addr")
 var flagTimeout = flag.Int("timeout", 3, "connect/read timeout")
 var flagType = flag.String("type", "local", "server_type:local or remote")
 var flagSvrPem = flag.String("svr_pem", "./server.pem", "pem file path")
 var flagSvrKey = flag.String("svr_key", "./server.key", "key file path")
-var flagUser = flag.String("user", "hellosen", "user name")
-var flagPwd = flag.String("pwd", "xxxxxxxx", "user pwd")
+var flagUser = flag.String("user", "xxxsen", "user name")
+var flagPwd = flag.String("pwd", "hello test", "user pwd")
 
 func buildConfig(config *enc_socks.ServerConfig) {
     config.LocalAddr = *flagLocal
@@ -35,8 +36,20 @@ func buildConfig(config *enc_socks.ServerConfig) {
     config.UserInfo = *authMap
 }
 
+func initLog() {
+    customFormatter := new(logrus.TextFormatter)
+    customFormatter.FullTimestamp = true                    // 显示完整时间
+    customFormatter.TimestampFormat = "2006-01-02 15:04:05" // 时间格式
+    customFormatter.DisableTimestamp = false                // 禁止显示时间
+    customFormatter.DisableColors = false                   // 禁止颜色显示
+    logrus.SetFormatter(customFormatter)
+    logrus.SetOutput(os.Stdout)
+    logrus.SetLevel(logrus.DebugLevel)
+}
+
 func main() {
     flag.Parse()
+    initLog()
     config := &enc_socks.ServerConfig{}
     buildConfig(config)
     logrus.Infof("Parse config:%s", config.String())
